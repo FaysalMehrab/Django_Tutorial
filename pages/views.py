8902a84eb5a817bb142134934382461b271b4859
MehrabChowdhury <mehrabdaishi974@gmail.com>
2026-01-20 21:16:00 +0600

    f

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 def home(request):
+    all_data = Post.objects.all()
     context = {
         'year' : 2026,
         'day' : 1,
+        'posts': all_data,
     }
 
     return render(request, 'home.html', context )
 
-# Create your views here.
+

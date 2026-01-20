62d376754394dd7c38e68b3c4bf1b640de5b0097
MehrabChowdhury <mehrabdaishi974@gmail.com>
2026-01-20 22:34:19 +0600

    h

--- pages/views.py
+++ pages/views.py
@@ -1,15 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 
 def home(request):
-    all_data = Post.objects.all()
+    all_posts = Post.objects.all()
     context = {
         'year' : 2026,
         'day' : 1,
-        'posts': all_data,
+        'posts': all_posts,
     }
 
     return render(request, 'home.html', context )
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk = pk)
+
+    context = {'post': post}
+
+    return render(request, 'post_detail.html', context)
 
 
